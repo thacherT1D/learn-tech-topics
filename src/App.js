0c50c4adf526abf2dbd91c643ab2816e5423a573
Thacher Hussain <thacherT1D@gmail.com>
2019-04-16 16:47:11 -0700

    adding common components from auth project, basic redux setup

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import {
+  Container,
+  Header,
+  Left,
+  Body,
+  Right,
+  Title,
+  Content,
+  Button,
+  Text } from 'native-base';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducers from './reducers';
 
 export default class App extends React.Component {
   render() {
     return (
-      <Provider store={createStore}>
-        <View style={styles.container}>
-          <Text>Open up App.js to start working on your app!</Text>
-        </View>
-      </Provider>
+      <Container>
+      <Header>
+        <Left/>
+        <Body>
+          <Title>Learn Tech</Title>
+        </Body>
+        <Right />
+      </Header>
+        <Content padder>
+          <Provider store={createStore(reducers)}>
+            <View style={styles.container}>
+              <Text>Open up App.js to start working on your app!</Text>
+            </View>
+          </Provider>
+        </Content>
+      </Container>
     );
   }
 }
